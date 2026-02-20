4716c0bc3dcae57daa035eec8b72e69921608cd8
Krishna Dani <krishnagdani@gmail.com>
2026-02-20 23:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (38.10%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for(int i = n - 1; i >= 0; i--) {
+
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;   // no carry needed
+            }
+
+            digits[i] = 0;  // if digit is 9
+        }
+
+        // if all digits were 9
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+}
