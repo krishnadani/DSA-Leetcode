7a15deb80cff36813fbc31ed1120f788f15a133d
Krishna Dani <krishnagdani@gmail.com>
2026-06-28 13:16:11 +0530

    Time: 516 ms (99.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,7 @@
+select round(count(distinct b.player_id)/count(distinct a.player_id),2) as fraction
+from (select player_id , min(event_date) as event_date
+      from Activity
+      group by player_id) a
+left join Activity b
+on a.player_id = b.player_id
+and datediff(b.event_date,a.event_date)=1;
